9ea536d92d015cd522f7d8a7e5bb61c15abd4622
Lala Tseng <z162614@gmail.com>
2024-12-13 18:12:32 +0800

    Removed department editing feature in AdminDepartmentPage.

--- src/components/DepartmentTable.jsx
+++ src/components/DepartmentTable.jsx
@@ -1,55 +1,20 @@
-import { Space, Table, Dropdown, Button } from "antd";
-import { FaTable, FaEdit } from "react-icons/fa";
-import { IoIosAdd } from "react-icons/io";
+import { Space, Table, Button } from "antd";
+import { FaTable } from "react-icons/fa";
 import PropTypes from  "prop-types"
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const { Column, ColumnGroup } = Table;
 
-const dropdownItems = [
-  {
-    label: <Button type="text">編輯</Button>,
-    key: "0",
-  },
-  {
-    type: "divider",
-  },
-  {
-    label: (
-      <Button type="text" danger>
-        刪除
-      </Button>
-    ),
-    key: "1",
-  },
-];
 
 const DepartmentTable = ({category, specialties}) => {
-  const [dataSource, setDataSource] = useState(specialties.map((s, index) => (
+  const dataSource = specialties.map((s, index) => (
     {
       key: index,
       specialty: s,
     }
-  )))
+  ))
   const navigate = useNavigate()
 
-  const handleClickEdit = (index) => {
-    console.log(index);
-  }
-  const handleAddSpecailty = () => {
-      const newData = {
-        key: specialties.length + 1,
-       specialty: "新科別",
-      };
-      setDataSource([...dataSource, newData]);
-  }
-  const handleDeleteSpecialty = (key) => {
-    const newData = dataSource.filter((data) => (
-     data.key !== key 
-    ))
-    setDataSource(newData);
-  };
   const handleClickSchedules = (record) => {
     navigate(`/admin/schedules/${record.specialty}`, {
       state: {
@@ -73,46 +38,8 @@ const DepartmentTable = ({category, specialties}) => {
               </Space>
             )}
           />
-          <Column
-            title="編輯"
-            key="action"
-            render={(_, record) => (
-              <Space size="middle">
-                <Dropdown
-                  menu={{
-                    items: dropdownItems,
-                    onClick: (items) => {
-                      switch (items.key) {
-                        case "0":
-                          handleClickEdit(record);
-                          break;
-                        case "1":
-                          handleDeleteSpecialty(record.key);
-                          break;
-                        default:
-                          break;
-                      }
-                    },
-                  }}
-                  trigger={["click"]}
-                >
-                  <button onClick={(e) => e.preventDefault()}>
-                    <FaEdit size={24} />
-                  </button>
-                </Dropdown>
-              </Space>
-            )}
-          />
         </ColumnGroup>
       </Table>
-      <Button
-        className="mb-4 text-gray-400"
-        type="text"
-        onClick={handleAddSpecailty}
-      >
-        <IoIosAdd size={24} />
-        新增科別
-      </Button>
     </div>
   );
 }
